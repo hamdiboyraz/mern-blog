84e55805bafe5e92097911ff8d92057d0c60113a
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-13 05:14:51 +0300

    Add routes for home and login page via react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,19 @@
 import "./App.css";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Post from "./components/Post";
+import Layout from "./components/Layout";
+import IndexPage from "./components/IndexPage";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          My Blog
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Hamdi</a>
-            <time>2023-01-12 19.45</time>
-          </p>
-          <p className="summary">
-            Today at its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products, including a “Whole-Home Backup
-            Power Solution.”
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Hamdi</a>
-            <time>2023-01-12 19.45</time>
-          </p>
-          <p className="summary">
-            Today at its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products, including a “Whole-Home Backup
-            Power Solution.”
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Hamdi</a>
-            <time>2023-01-12 19.45</time>
-          </p>
-          <p className="summary">
-            Today at its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products, including a “Whole-Home Backup
-            Power Solution.”
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+    </Routes>
   );
 }
 
