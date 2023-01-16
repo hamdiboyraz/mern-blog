62e430b32a34c022886b9907aa5ac87732f11ab8
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-16 23:20:21 +0300

    Add create post page on frontend

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'image'],
+    ['clean'],
+  ],
+};
+
+const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
+
+  const createNewPost = (e) => {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('files', files[0]); // files is an array of files
+    e.preventDefault();
+    fetch('http://localhost:4000/post', {
+      method: 'POST',
+      body: data,
+    });
+  };
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Summary"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button style={{ marginTop: '5px' }}>Create Post</button>
+    </form>
+  );
+};
+
+export default CreatePost;
