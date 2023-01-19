6218547303610cf42bfc497458d0f80208648979
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-19 05:08:22 +0300

    Add indexpage

--- client/src/components/IndexPage.js
+++ client/src/components/IndexPage.js
@@ -1,14 +1,24 @@
-import React from "react";
-import Post from "./Post";
+import React from 'react';
+import Post from './Post';
+import { useEffect } from 'react';
 
 const IndexPage = () => {
-  return (
-    <>
-      <Post />
-      <Post />
-      <Post />
-    </>
-  );
+  const [posts, setPosts] = React.useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/post')
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     const res = await fetch('http://localhost:4000/post');
+    //     const data = await res.json();
+    //     setPosts(data);
+    //   };
+
+    //   fetchData();
+  }, []);
+
+  return <>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</>;
 };
 
 export default IndexPage;
